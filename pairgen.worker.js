6bcf17a206b20eb93c8176b9af407ad59bb48e29
SHIN Suzuki <shinout310@gmail.com>
2011-08-09 17:23:54 +0900

    check if distance of pairs are valid

--- pairgen.worker.js
+++ pairgen.worker.js
@@ -7,21 +7,21 @@ const FASTAReader   = require(__dirname + '/lib/FASTAReader/FASTAReader');
 const fs            = require('fs');
 
 function Pairgen(config, make_contig) {
-	const self = this;
+  const self = this;
 
-	// random [0 -1) function
+  // random [0 -1) function
   this.random = new XORShift(config.para_id, true); // function
 
-	// quality. IIIIIIIIIII............ 
+  // quality. IIIIIIIIIII............ 
   this.qual   = (function(){ 
-		var a = '';
-		for(var i = 0, l = config.readlen; i<l; i++) {
-			a += 'I';
-		} 
-		return a;
-	})();
+    var a = '';
+    for(var i = 0, l = config.readlen; i<l; i++) {
+      a += 'I';
+    } 
+    return a;
+  })();
 
-	// fastas
+  // fastas
   this.fastas = new FASTAReader(config.path);
 
   // weighted selection of rnames
@@ -33,20 +33,20 @@ function Pairgen(config, make_contig) {
     return ret;
   })() , this.random); 
 
-	// total iteration
+  // total iteration
   this.times  = Math.floor(this.rselect.total() * config.depth / (2 * config.readlen) / config.parallel + 0.5);
-	this.config = config;
-	this.flags  = {l : true, r : true};
+  this.config = config;
+  this.flags  = {l : true, r : true};
 
 
   /* files to write */
   this.left_file  = fs.createWriteStream(config.tmp_dir + '/left_' + config.para_id, {
-		bufferSize: 40960, encoding: 'utf-8', flags: 'w'
-	});
+    bufferSize: 40960, encoding: 'utf-8', flags: 'w'
+  });
 
   this.right_file = fs.createWriteStream(config.tmp_dir + '/right_' + config.para_id, {
-		bufferSize: 40960, encoding: 'utf-8', flags: 'w'
-	});
+    bufferSize: 40960, encoding: 'utf-8', flags: 'w'
+  });
 
 
   this.left_file.on('drain', function() { 
@@ -59,41 +59,41 @@ function Pairgen(config, make_contig) {
     self.run(); 
   });
 
-	this.i = 0;
+  this.i = 0;
   this.running = false;
 
-	Object.seal(this);
+  Object.seal(this);
 }
 
 Pairgen.prototype.run = function() {
-	const width    = this.config.width;
-	const dev      = this.config.width;
-	const random   = this.random;
-	const readlen  = this.config.readlen;
-	const rselect  = this.rselect;
-	const qual     = this.qual;
-	const fastas   = this.fastas;
-	const ms       = this.config.modify_seq;
-	const mq       = this.config.modify_qual;
-	const gfid     = this.config.get_fragment_id;
-	const left_id  = this.config.pair_id[0];
-	const right_id = this.config.pair_id[1];
+  const width    = this.config.width;
+  const dev      = this.config.width;
+  const random   = this.random;
+  const readlen  = this.config.readlen;
+  const rselect  = this.rselect;
+  const qual     = this.qual;
+  const fastas   = this.fastas;
+  const ms       = this.config.modify_seq;
+  const mq       = this.config.modify_qual;
+  const gfid     = this.config.get_fragment_id;
+  const left_id  = this.config.pair_id[0];
+  const right_id = this.config.pair_id[1];
 
   if (this.running) return;
   this.running = true;
-	while (this.i < this.times) {
-		if (!this.flags.l || !this.flags.r) {
+  while (this.i < this.times) {
+    if (!this.flags.l || !this.flags.r) {
       this.running = false;
       return;
     }
 
-		var rname   = rselect.random();
-		var baselen = fastas.result[rname].getEndPos();
+    var rname   = rselect.random();
+    var baselen = fastas.result[rname].getEndPos();
     do {
       var distance = Math.floor(norm_rand(width, dev, random) + 0.5);
-    } while ( baselen - distance - readlen * 2 <= 0);
+    } while (distance < - readlen || baselen - distance - readlen * 2 <= 0);
 
-		// limit position equals max - distance - readlen * 2
+    // limit position equals max - distance - readlen * 2
     var range     = baselen - distance - readlen * 2;
     var startpos  = 1 + Math.floor(random() * (range-1) + 0.5);
     var startpos2 = startpos + readlen + distance;
@@ -105,24 +105,24 @@ Pairgen.prototype.run = function() {
     if (fastas.hasN(rname, startpos2, readlen)) continue;
 
     this.flags.l = this.left_file.write(
-			'@' + flg_id  + left_id + '\n' + 
-			leftread  + '\n' + 
-			'+\n' + 
-			mq.call(this, qual, leftread) + '\n'
-		);
+      '@' + flg_id  + left_id + '\n' + 
+      leftread  + '\n' + 
+      '+\n' + 
+      mq.call(this, qual, leftread) + '\n'
+    );
 
     this.flags.r = this.right_file.write(
-			'@' + flg_id + right_id + '\n' + 
-			rightread + '\n' +
-			'+\n' + 
-			mq.call(this, qual, rightread) + '\n'
-		);
+      '@' + flg_id + right_id + '\n' + 
+      rightread + '\n' +
+      '+\n' + 
+      mq.call(this, qual, rightread) + '\n'
+    );
 
     this.i++;
-	}
+  }
 
-	// on end
-	this.config.callback(this);
+  // on end
+  this.config.callback(this);
 };
 
 
@@ -133,12 +133,12 @@ onmessage = function(msg) {
   };
 
   var pairgen = new Pairgen(config);
-	pairgen.run();
+  pairgen.run();
 };
 
 onerror = function(e) {
-	console.log(e.stack);
-	this.close();
+  console.log(e.stack);
+  this.close();
 };
 
 
